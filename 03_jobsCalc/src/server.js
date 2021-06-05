fb28c75f4229b708431a64dd892218e6bc36fda0
FelipePDS <felipepdasilva66@gmail.com>
2021-06-04 21:50:15 -0300

    configuring server to use ejs

--- 03_jobsCalc/src/server.js
+++ 03_jobsCalc/src/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+
 const router = require('./router');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
